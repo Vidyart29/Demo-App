0fb86abc1c3a97c1e2d159515d7ab057c1ccc024
Vidya Rautela <vane28ssavdr@gmail.com>
2025-05-17 18:42:30 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,7 +1,7 @@
 # script to calculate body fat
 
 from fitness_tools.composition.bodyfat import DurninWomersley
-data = DurninWomersley(24, 'female', (5, 7, 8, 12))
+data = DurninWomersley24, 'female', (5, 7, 8, 12))
 data.body_density()
 print("body fat percentage: " + str(data) )
 print("script executed successfully: v1")
