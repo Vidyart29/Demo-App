721fc3f0549280049ef920265434bb092ee5db48
Vidya Rautela <vane28ssavdr@gmail.com>
2025-05-17 18:25:53 +0530

    Update script.py

--- script.py
+++ script.py
@@ -4,4 +4,4 @@ from fitness_tools.composition.bodyfat import DurninWomersley
 data = DurninWomersley(24, 'female', (5, 7, 8, 12))
 data.body_density()
 print("body fat percentage: " + str(data) )
-print("script executed successfully")
+print("script executed successfully: v1")
